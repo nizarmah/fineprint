eb9199ab27dd84bd0112c238d5abf7dabf65f840
Nizar Mahmoud <nizar.mah99@gmail.com>
2020-04-11 23:34:10 +0300

    Fix: Moved Rules and Condition Objects to Page Rules JS File
    It is only reasonable that that is done, because those objects are being used by that file and are related to the Page Rules
    
    Meanwhile, the validator JS file is only important to validate whether a rule or a condition is satisfied by a page

--- page_rules.js
+++ page_rules.js
@@ -13,3 +13,31 @@ var pageRules = [
         ]
     })
 ];
+
+function Condition(condition) {
+    if (condition.pageUrl) {
+        this.pageUrl = condition.pageUrl || {};
+    }
+
+    if (condition.css) {
+        this.css = condition.css || [];
+    }
+}
+
+function Rule(rule) {
+    this.conditions = rule.conditions || [];
+
+    this.getCondition = function (index) {
+        return this.conditions[index];
+    };
+
+    this.cssFiles = rule.cssFiles || [];
+
+    this.getCssFiles = function () {
+        return this.cssFiles;
+    };
+}
+
+function getRule(ruleIndex) {
+    return pageRules[ruleIndex];
+}
